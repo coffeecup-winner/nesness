86b84cfcb100b0552130b301fbee70ecfb65f2cb
Dmitry Polyanitsa <coffeecup.winner@gmail.com>
2021-09-14 18:42:51 +0300

    Rewrite PPU to use registers instead of higher level logic

--- src/ppu/ppu.rs
+++ src/ppu/ppu.rs
@@ -12,10 +12,15 @@ pub struct PPU {
     pub current_scanline: u16,
     pub current_cycle: u16, // within a scanline
 
+    // Internal registers
+    reg_v: Cell<u16>,  // Current VRAM address
+    reg_t: u16,        // Temp VRAM address
+    reg_x: u8,         // Fine X scroll
+    reg_w: Cell<bool>, // Write toggle
+
     latch: u8,
 
     // PPU control
-    nametable_base_addr: u16,
     sprite_pattern_table_addr: u16,
     background_pattern_table_addr: u16,
     vram_incr: u8,
@@ -44,15 +49,6 @@ pub struct PPU {
     // OAM data
     oam_data: [u8; 256],
 
-    // PPU scroll
-    scroll_x: Cell<u8>,
-    scroll_y: Cell<u8>,
-    wrote_scroll_x: Cell<bool>,
-
-    // PPU address
-    ppu_addr: Cell<u16>,
-    wrote_ppu_addr_hi: Cell<bool>,
-
     // PPU data (VRAM)
     ppu_vram: Vec<u8>,
 
@@ -65,8 +61,11 @@ impl PPU {
         PPU {
             current_scanline: 0,
             current_cycle: 0,
+            reg_v: Cell::new(0),
+            reg_t: 0,
+            reg_x: 0,
+            reg_w: Cell::new(false),
             latch: 0,
-            nametable_base_addr: 0x2000,
             sprite_pattern_table_addr: 0x0000,
             background_pattern_table_addr: 0x0000,
             vram_incr: 1,
@@ -86,11 +85,6 @@ impl PPU {
             is_in_vblank: Cell::new(false),
             oam_addr: Cell::new(0),
             oam_data: [0; 256],
-            scroll_x: Cell::new(0),
-            scroll_y: Cell::new(0),
-            wrote_scroll_x: Cell::new(false),
-            ppu_addr: Cell::new(0),
-            wrote_ppu_addr_hi: Cell::new(false),
             ppu_vram: vec![0; 0x4000],
             oam_dma_page: 0,
         }
@@ -115,7 +109,7 @@ impl PPU {
     pub fn write_ppuctrl(&mut self, value: u8) {
         self.latch = value;
         // TODO: ignore writes for 30k cycles (?)
-        self.nametable_base_addr = 0x2000 + 0x400 * (value & 0x03) as u16;
+        self.reg_t = (self.reg_t & !0x0c00) | (((value & 0x03) as u16) << 10);
         self.vram_incr = if (value & 0x04) == 0 { 1 } else { 32 };
         self.sprite_pattern_table_addr = if (value & 0x08) == 0 { 0x0000 } else { 0x1000 };
         self.background_pattern_table_addr = if (value & 0x10) == 0 { 0x0000 } else { 0x1000 };
@@ -158,11 +152,7 @@ impl PPU {
             result |= 0x80;
         }
         self.is_in_vblank.set(false);
-        self.scroll_x.set(0);
-        self.scroll_y.set(0);
-        self.wrote_scroll_x.set(false); // is this correct?
-        self.ppu_addr.set(0);
-        self.wrote_ppu_addr_hi.set(false); // is this correct?
+        self.reg_w.set(false);
         result
     }
 
@@ -202,12 +192,15 @@ impl PPU {
 
     pub fn write_ppuscroll(&mut self, value: u8) {
         self.latch = value;
-        if self.wrote_scroll_x.get() {
-            self.scroll_y.set(value);
+        if !self.reg_w.get() {
+            self.reg_t = (self.reg_t & !0x001f) | ((value as u16) >> 3);
+            self.reg_x = value & 0x7;
         } else {
-            self.scroll_x.set(value);
+            self.reg_t = (self.reg_t & !0x73e0)
+                | (((value as u16) << 13) >> 1)
+                | (((value as u16) >> 3) << 5);
         }
-        self.wrote_scroll_x.set(!self.wrote_scroll_x.get());
+        self.reg_w.set(!self.reg_w.get());
     }
 
     pub fn read_ppuaddr(&self) -> u8 {
@@ -217,20 +210,20 @@ impl PPU {
 
     pub fn write_ppuaddr(&mut self, value: u8) {
         self.latch = value;
-        if self.wrote_ppu_addr_hi.get() {
-            self.ppu_addr
-                .set(self.ppu_addr.get() & 0xff00 | (value as u16));
+        if !self.reg_w.get() {
+            self.reg_t = (self.reg_t & 0x00ff) | (((value as u16) & 0x3f) << 8);
         } else {
-            self.ppu_addr.set((value as u16) << 8);
+            self.reg_t = (self.reg_t & 0xff00) | (value as u16);
+            self.reg_v.set(self.reg_t);
         }
-        self.wrote_ppu_addr_hi.set(!self.wrote_ppu_addr_hi.get());
+        self.reg_w.set(!self.reg_w.get());
     }
 
     pub fn read_ppudata(&self) -> u8 {
         // TODO: use internal buffer instead of the direct read
         let result = self.ppu_vram[self.get_ppu_addr()];
-        self.ppu_addr
-            .set(self.ppu_addr.get().wrapping_add(self.vram_incr as u16));
+        self.reg_v
+            .set(self.reg_v.get().wrapping_add(self.vram_incr as u16));
         result
     }
 
@@ -238,8 +231,8 @@ impl PPU {
         self.latch = value;
         let addr = self.get_ppu_addr();
         self.ppu_vram[addr] = value;
-        self.ppu_addr
-            .set(self.ppu_addr.get().wrapping_add(self.vram_incr as u16));
+        self.reg_v
+            .set(self.reg_v.get().wrapping_add(self.vram_incr as u16));
     }
 
     pub fn read_oamdma(&self) -> u8 {
@@ -254,12 +247,12 @@ impl PPU {
     }
 
     fn get_ppu_addr(&self) -> usize {
-        let addr = self.ppu_addr.get();
-        (match addr & 0x3fff {
+        let addr = self.reg_v.get();
+        (match addr {
             0x0000..=0x2fff => addr,
             0x3000..=0x3eff => addr - 0x1000,
             0x3f00..=0x3fff => 0x3f00 | (addr & 0x1f),
-            _ => unreachable!(),
+            _ => panic!("Wrong register v setting somewhere"),
         }) as usize
     }
 }
